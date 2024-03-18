e0c9626f0abb64ec2493d7569cf60adf0aae04e2
vsgirelli <vavi_sg@hotmail.com>
2024-03-18 15:14:23 +0100

    Updating gitignore and adding first code

---
+++ main.R
@@ -0,0 +1,16 @@
+# #########
+# SMDE 1st Assignment
+#
+# Authors:
+#    Javier Beiro Piñón
+#    Valéria Soldera Girelli
+#    Duru Degimli
+# #########
+
+laptop_data <- read.csv("data/laptop_data_cleaned.csv", header=TRUE, stringsAsFactors=FALSE)
+
+# The data set consists of following variables: Company, Type Name, Ram, Weight, Price, Touch Screen
+# indicator, IPS indicator, Ppi, Cpu_brand, HDD, SSD, Gpu_brand, Os (Operation System).
+
+# a) Import data set to R assigning the type of each variable correctly. (5p)
+
