82d19666e53306644d8c50270c9098741825311d
jabepi <javier.beiro@hotmail.com>
2024-03-20 16:27:32 +0100

    a and b

--- main.R
+++ main.R
@@ -7,10 +7,27 @@
 #    Duru Degimli
 # #########
 
-laptop_data <- read.csv("data/laptop_data_cleaned.csv", header=TRUE, stringsAsFactors=FALSE)
-
-# The data set consists of following variables: Company, Type Name, Ram, Weight, Price, Touch Screen
-# indicator, IPS indicator, Ppi, Cpu_brand, HDD, SSD, Gpu_brand, Os (Operation System).
 
 # a) Import data set to R assigning the type of each variable correctly. (5p)
+# Importing the dataset
+laptop_data <- read.csv("data/laptop_data_cleaned.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# Converting numeric variables
+laptop_data$Ram <- as.numeric(laptop_data$Ram)
+laptop_data$Weight <- as.numeric(laptop_data$Weight)
+laptop_data$Price <- as.numeric(laptop_data$Price)
+laptop_data$Ppi <- as.numeric(laptop_data$Ppi)
+laptop_data$HDD <- as.numeric(laptop_data$HDD)
+laptop_data$SSD <- as.numeric(laptop_data$SSD)
+
+# Converting boolean variables (assuming they are currently stored as integers 0 and 1 or as characters 'Yes'/'No')
+laptop_data$TouchScreen <- as.logical(laptop_data$TouchScreen)
+laptop_data$Ips <- as.logical(laptop_data$Ips)
+
+
+# b) Create a dataset including only types of laptops: Ultrabook, Notebook and 2 in 1 Convertible.
+filtered_laptop_data <- subset(laptop_data, TypeName %in% c("Ultrabook", "Notebook", "2 in 1 Convertible"))
+
+
+
 
