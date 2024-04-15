a440831149f5702b6273a452a9d1f5407b3e4316
vsgirelli <vavi_sg@hotmail.com>
2024-04-15 17:42:40 +0200

    Improving the removal of outliers (which effectively affects the rest of the model)

--- question3.R
+++ question3.R
@@ -20,7 +20,48 @@ laptop$HDD <- as.numeric(laptop$HDD)
 laptop$SSD <- as.numeric(laptop$SSD)
 
 ###### Removing outliers
-laptop <- laptop[- which(laptop$Ram == 64), ]
+
+boxplot(laptop$SSD,
+        main = "Box Plot of Laptop Weights",
+        ylab = "Ram",
+        xlab = "Laptops",
+        col = "lightblue", # Set color
+        border = "darkblue",
+        cex.main=3.0, # Double the main title text size (previously 1.5)
+        cex.lab=2.6, # Double the axis labels text size (previously 1.3)
+        cex.axis=2.2) # Double the axis annotation text size (previously 1.1)
+
+# Copy the original dataset to a new variable for filtering
+laptop_subset_filtered <- laptop
+
+# Initiate a counter for tracking
+iteration_counter <- 1
+repeat {
+  # Calculate outliers in the filtered dataset
+  outliers <- boxplot.stats(laptop_subset_filtered$Weight)$out
+  # Print the iteration and number of outliers found (optional)
+  cat("Iteration", iteration_counter, ": Found", length(outliers), "outliers\n")
+  
+  # Break the loop if no outliers are found
+  if(length(outliers) == 0) {
+    break
+  }
+  # Filter out the outliers from the filtered dataset
+  laptop_subset_filtered <- laptop_subset_filtered[!laptop_subset_filtered$Weight %in% outliers, ]
+  # Increment the counter
+  iteration_counter <- iteration_counter + 1
+}
+
+boxplot(laptop_subset_filtered$Ram,
+        main = "Box Plot of Laptop Weights",
+        ylab = "Ram",
+        xlab = "Laptops",
+        col = "lightblue", # Set color
+        border = "darkblue",
+        cex.main=3.0, # Double the main title text size (previously 1.5)
+        cex.lab=2.6, # Double the axis labels text size (previously 1.3)
+        cex.axis=2.2) # Double the axis annotation text size (previously 1.1)
+
 
 # Since we need to find the best *simple linear regression model*
 # we have to assess each numerical variable separately to determine 
@@ -34,18 +75,21 @@ laptop <- laptop[- which(laptop$Ram == 64), ]
 #install.packages("corrplot")
 # Correlation analysis
 library(corrplot)
-summary(laptop)
-num_laptop <- laptop[, sapply(laptop, is.numeric)]
+summary(laptop_subset_filtered)
+num_laptop <- laptop_subset_filtered[, sapply(laptop_subset_filtered, is.numeric)]
 correlation_matrix <- cor(num_laptop)
 corrplot(correlation_matrix, method = "color", tl.col = "black", tl.cex = 1, cl.cex = 0.8)
 # In this corrplot, basically it means that the darker blue colors (essentially with RAM and SSD)
 # indicate that there's a strong positive relationship
 # Closer to white indicate no correlation
-# I don't really understand the "negative relationship", but that would be the red colors
-# (essentially with HDD and PPI). 
+# Negative relationship would be the red colors,
+# means that as the value of one variable increases, the value of the other variable tends to decrease.
+# We don't have this case.
 # These correlation values can also be seen in the matrix below.
 # The correlation values are important to determine whether a relationship might exist.
+
 correlation_with_price <- correlation_matrix["Price", -ncol(correlation_matrix)]
+remaining_columns <- colnames(correlation_matrix)[-which(colnames(correlation_matrix) == "Price")]
 barplot(correlation_with_price, main = "Correlation with Price", names.arg = names(correlation_with_price))
 print(correlation_with_price)
 
@@ -57,36 +101,36 @@ library(ggplot2)
 
 
 # Outliers: the right-most point is an outlier
-ram_price <- ggplot(laptop, aes_string(x = "Ram", y = "Price")) +
+ram_price <- ggplot(laptop_subset_filtered, aes_string(x = "Ram", y = "Price")) +
     theme(text = element_text(size = 16)) +
     scale_y_continuous(labels = scales::number_format(accuracy = 0.01),
-                       breaks = seq(min(laptop$Price), max(laptop$Price), length.out = 5)) +
+                       breaks = seq(min(laptop_subset_filtered$Price), max(laptop_subset_filtered$Price), length.out = 5)) +
     scale_x_continuous(labels = scales::number_format(accuracy = 0.01),
-                       breaks = seq(min(laptop$Ram), max(laptop$Ram), length.out = 7)) +
+                       breaks = seq(min(laptop_subset_filtered$Ram), max(laptop_subset_filtered$Ram), length.out = 7)) +
     geom_point() +
     labs(x = "Ram", y = "Price") +
     ggtitle(paste( "Ram vs. Price"))
 print(ram_price)
 
-weight_price <- ggplot(laptop, aes_string(x = "Weight", y = "Price")) +
+weight_price <- ggplot(laptop_subset_filtered, aes_string(x = "Weight", y = "Price")) +
   theme(text = element_text(size = 16)) +
   geom_point() +
   labs(x = "Weight", y = "Price") +
   ggtitle(paste( "Weight vs. Price"))
 print(weight_price)
 
-ppi_price <- ggplot(laptop, aes_string(x = "Ppi", y = "Price")) +
+ppi_price <- ggplot(laptop_subset_filtered, aes_string(x = "Ppi", y = "Price")) +
   theme(text = element_text(size = 16)) +
   scale_y_continuous(labels = scales::number_format(accuracy = 0.01),
-                     breaks = seq(min(laptop$Price), max(laptop$Price), length.out = 5)) +
+                     breaks = seq(min(laptop_subset_filtered$Price), max(laptop_subset_filtered$Price), length.out = 5)) +
   scale_x_continuous(labels = scales::number_format(accuracy = 1),
-                     breaks = seq(min(laptop$Ppi), max(laptop$Ppi), length.out = 7)) +
+                     breaks = seq(min(laptop_subset_filtered$Ppi), max(laptop_subset_filtered$Ppi), length.out = 7)) +
   geom_point() +
   labs(x = "Ppi", y = "Price") +
   ggtitle(paste( "Ppi vs. Price"))
 print(ppi_price)
 
-hdd_price <- ggplot(laptop, aes_string(x = "HDD", y = "Price")) +
+hdd_price <- ggplot(laptop_subset_filtered, aes_string(x = "HDD", y = "Price")) +
   theme(text = element_text(size = 16)) +
   geom_point() +
   labs(x = "HDD", y = "Price") +
@@ -94,45 +138,26 @@ hdd_price <- ggplot(laptop, aes_string(x = "HDD", y = "Price")) +
 print(hdd_price)
 
 # Outliers: should we consider the point 750 as outlier?
-ssd_price <- ggplot(laptop, aes_string(x = "SSD", y = "Price")) +
+ssd_price <- ggplot(laptop_subset_filtered, aes_string(x = "SSD", y = "Price")) +
   theme(text = element_text(size = 16)) +
   geom_point() +
   labs(x = "SSD", y = "Price") +
   ggtitle(paste( "SSD vs. Price"))
 print(ssd_price)
 
-# Normality test (over raw values)
-print(names(laptop)[sapply(laptop, is.numeric)])
-# Ram
-ram_shapiro <- shapiro.test(laptop$Ram)
-ram_p_value <- ram_shapiro$p.value # normal
-# Weight
-weight_shapiro <- shapiro.test(laptop$Weight)
-weight_p_value <- weight_shapiro$p.value
-# Ppi
-ppi_shapiro <- shapiro.test(laptop$Ppi)
-ppi_p_value <- ppi_shapiro$p.value
-# HDD
-hdd_shapiro <- shapiro.test(laptop$HDD)
-hdd_p_value <- hdd_shapiro$p.value
-# SSD
-ssd_shapiro <- shapiro.test(laptop$SSD)
-ssd_p_value <- ssd_shapiro$p.value # normal
-
-
 # MODELS
-mean_price <- mean(laptop$Price)
+mean_price <- mean(laptop_subset_filtered$Price)
 
 ################ RAM MODEL
 # (outlier removed)
+#ram_2 <- (num_laptop$Ram)^2
 model_ram <- lm(Price ~ Ram, data = num_laptop)
 # first I'll test if the method is valid by testing if the residuals:
 # 1. Its probability distribution is normal. (the mean of the dist is 0) (slide 53)
 qqnorm(residuals(model_ram))
 hist(residuals(model_ram))
-shapiro.test(residuals(model_ram))
-# p-value = 0.0414
-# All three indicate normality
+shapiro.test(residuals(model_ram)) # should be higher than confidence level
+# p-value = 0.252, normal
 
 # 2. The standard deviation is a constant regardless of the value of x (slide 57)
 plot(residuals(model_ram))
@@ -142,71 +167,31 @@ abline(h = 0, col = "red")
 # an outlier in a residual plot used to test for homoscedasticity
 # may indicate potential issues with the assumption of constant variance of the residuals.
 library(lmtest)
-bptest(model_ram)
-# We observe in the plot that there seems to be an outlier,
-# and the p-value of the Breusch-Pagan test is too small (3.393e-11),
-# This indicates evidence of heteroscedasticity, suggesting that the 
-# variance of the residuals varies across different values of the independent variables.
-# After removing the outlier:
+bptest(model_ram) # should be higher than the confidence level to indicate homocedasticity 
 # We have homocedasticity as the variance of the error is constant
-# and the p-value of the Breusch-Pagan test is 0.5586
+# and the p-value of the Breusch-Pagan test is 0.9558
 
 # 3. The error values are independent (59)
-dwtest(model_ram, alternative = "two.sided")
-# the Durbin-Watson p-value is 0.4986 indicates that there is no
-# autocorrelation in the residuals as it is higher than the significance level.
+dwtest(model_ram, alternative = "two.sided") # should be higher than the confidence level 
+# the Durbin-Watson p-value is 0.02219 indicates that there is
+# autocorrelation in the residuals as it is smaller than the significance level.
+# Tried modifications, didn't work
+
 
 # We need to make an analysis of the residuals: examine the differences
 # between the actual data points and those predicted by the linear equation
 summ_ram <- summary(model_ram)
 print(summ_ram)
-# The Residual standard error: 0.4447 on 1270 degrees of freedom
+# Residual standard error: 0.4448 on 1269 degrees of freedom
 # the Price's mean is 10.82, so since the error is small in comparison
 # to the price's mean, it means that the model is good.
-# For analyzing the slope, we get that the p-value is almost 0 (<2e-16)
+# For analyzing the slope, we get that the p-value is almost 0 (<2.2e-16)
 # so it indicates that we can reject the null hypothesis that the slope 
 # is zero and conclude that there is evidence of a linear relationship 
 # between the two variables.
-
-################ SSD MODEL
-model_ssd <- lm(Price ~ SSD, data = num_laptop)
-# first I'll test if the method is valid by testing if the residuals:
-# 1. Its probability distribution is normal. (the mean of the dist is 0) (slide 53)
-qqnorm(residuals(model_ssd))
-hist(residuals(model_ssd))
-shapiro.test(residuals(model_ssd))
-# p-value = 0.07079 TODO HERE it's not normal
-
-# 2. The standard deviation is a constant regardless of the value of x (slide 57)
-plot(residuals(model_ssd))
-abline(h = 0, col = "red")
-library(lmtest)
-bptest(model_ssd)
-# We have homocedasticity as the variance of the error is constant
-# and the p-value of the Breusch-Pagan test is 0.05876
-
-# 3. The error values are independent (59)
-dwtest(model_ssd, alternative = "two.sided")
-# the Durbin-Watson p-value is 0.6523 indicates that there is no
-# autocorrelation in the residuals.
-
-# We need to make an analysis of the residuals: examine the differences
-# between the actual data points and those predicted by the linear equation
-summ_ssd <- summary(model_ssd)
-print(summ_ssd)
-# Residual standard error: 0.4663 on 1271 degrees of freedom
-# the Price's mean is 10.82, so since the error is small in comparison
-# to the price's mean, it means that the model is good.
-# For analyzing the slope, we get that the p-value is almost 0 (<2e-16)
-# so it indicates that we can reject the null hypothesis that the slope 
-# is zero and conclude that there is evidence of a linear relationship 
-# between the two variables.
-
-
-
 
 # Coefficient of Determination: the strength of the relationship
-# Multiple R-squared:  0.4631 indicates that 46% of the variation of
+# Multiple R-squared:  0.4832 indicates that 48% of the variation of
 # the price is explained by the RAM.
 
 # TODO 
@@ -215,19 +200,58 @@ print(summ_ssd)
 
 
 
+################ SSD MODEL
+ssd_2 <- sqrt(num_laptop$SSD)
+
+model_ssd <- lm(Price ~ SSD+ssd_2, data = num_laptop)
+# first I'll test if the method is valid by testing if the residuals:
+# 1. Its probability distribution is normal. (the mean of the dist is 0) (slide 53)
+qqnorm(residuals(model_ssd))
+hist(residuals(model_ssd))
+shapiro.test(residuals(model_ssd))
+# p-value = 0.00624 isn't normal
+
+
+# 2. The standard deviation is a constant regardless of the value of x (slide 57)
+plot(residuals(model_ssd))
+abline(h = 0, col = "red")
+library(lmtest)
+bptest(model_ssd) # should be higher than the confidence level to indicate homocedasticity 
+# We have homocedasticity as the variance of the error is constant
+# and the p-value of the Breusch-Pagan test is 0.07201
+
+# 3. The error values are independent (59)
+dwtest(model_ssd, alternative = "two.sided") # should be higher than the confidence level 
+# the Durbin-Watson p-value is 0.4353 indicates that there is no
+# autocorrelation in the residuals.
+
+# We need to make an analysis of the residuals: examine the differences
+# between the actual data points and those predicted by the linear equation
+summ_ssd <- summary(model_ssd)
+print(summ_ssd)
+# Residual standard error: 0.4661 on 1269 degrees of freedom
+# the Price's mean is 10.82, so since the error is small in comparison
+# to the price's mean, it means that the model is good.
+# For analyzing the slope, we get that the p-value is almost 0 (<2.2e-16)
+# so it indicates that we can reject the null hypothesis that the slope 
+# is zero and conclude that there is evidence of a linear relationship 
+# between the two variables.
 
 
 
-# Model (and then normality test over residuals)
-#  Ram      Weight       Price         Ppi         HDD 
-# 0.68051892  0.15138619  1.00000000  0.48068692 -0.09736141 
-models <- lapply(names(correlation_with_price), function(var) lm(Price ~ ., data = num_laptop[, c(var, "Price")]))
-summary_stats <- lapply(models, summary)
 
-models <- lapply(names(correlation_with_price), function(var) lm(formula = Price ~ ., data = num_laptop))
+
+
+
+
+# Generating a model for all the other numerical variables and analysing
+# their Coefficient of Determination to check that the most significant ones
+# were RAM and SSD. the other ones present small Coefficient of Determination,
+# meaning that the strength of the relationship is smaller.
+models <- lapply(remaining_columns, function(var) lm(Price ~ ., data = num_laptop[, c(var, "Price")]))
+#summary_stats <- lapply(models, summary)
+#print(summary_stats)
 rsquared_values <- sapply(summary_stats, function(model_summary) model_summary$r.squared)
-
-# Select the best model
 best_model_index <- which.max(rsquared_values)
 best_model <- models[[best_model_index]]
 best_model_summary <- summary(best_model)
